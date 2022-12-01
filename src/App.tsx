a8ff93db4dda3b15fdef78521751ca26118c291d
GolyshMariia <g.mv@mail.ru>
2022-12-01 18:54:17 +0200

    folder assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import HomeMyComponent from './components/home/homeMyComponent';
 //import ClickCount from './components/clickCount';
+import image from './Assets/myimg.png'
 
 
 const App: React.FC = () => {
   return (
     <div>
       <HomeMyComponent />
-      
+      <img src={image} alt='my image' />
     </div>
   )
 }
