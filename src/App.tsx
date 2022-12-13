8b55ca29e0098623659b9461061185c6574b7b1e
GolyshMariia <g.mv@mail.ru>
2022-12-13 09:31:23 +0200

    fix erorr and push

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import HomeMyComponent from './components/home/homeMyComponent';
 //import ClickCount from './components/clickCount';
-import pic from './Assets/myimg.png';
+//import pic from './Assets/myimg.png';
 
 
 const App: React.FC = () => {
   return (
     <div>
       <HomeMyComponent />
-      <img src={pic} alt="my image" />
     </div>
   )
 }
