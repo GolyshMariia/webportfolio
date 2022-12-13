b4524b57282c5e26fa136840ca8497232735c9b1
GolyshMariia <g.mv@mail.ru>
2022-12-13 12:27:01 +0200

    test animimscss home

--- src/components/home/homeMyComponent.tsx
+++ src/components/home/homeMyComponent.tsx
@@ -9,8 +9,10 @@ type HomeMyComponentProps = {
 const HomeMyComponent: React.FC<HomeMyComponentProps> = () => {
   return (
     <div className={styles.container}>
-  <h1>Welcome to my page</h1>
-  <p>Mariia Golysh</p>
+      <h1>
+        <span>Welcome to my page</span>
+        </h1>
+  <p>Maria Golysh</p>
 </div>
   )
 }
